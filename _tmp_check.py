5fab75a436bbdd35d8f5ea09ae1c106c6f1c7f19
raozihao <2246339663@qq.com>
2026-03-04 17:51:37 +0800

    清理: 删除24个临时修复/调试脚本（含明文密码，不应留在仓库）

--- _tmp_check.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-c.connect('38.55.129.237', 22, 'root', 'vzdWC9pN8HkT', timeout=15)
-cmd = """docker exec govai-postgres psql -U govai_user -d govai_db -c "SELECT id, name, entity_type FROM graph_entities LIMIT 5;" """
-i, o, e = c.exec_command(cmd)
-print(o.read().decode())
-print(e.read().decode())
-c.close()
